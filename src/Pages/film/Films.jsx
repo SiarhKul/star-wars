3867cc865f37d7cdeb776c5129336711096e03e1
Sergey Kulazhenko <skulazhanka@exadel.com>
2021-07-19 13:02:27 +0300

    feat: Added a handler Error

--- src/Pages/film/Films.jsx
+++ src/Pages/film/Films.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { bodyCardFilm } from "../../components/bodyCards/bodyCardFIlm";
 import { withCard } from "../../hoc/withCardPeople";
+import { ToastContainer } from "react-toastify";
 
 const FilmsCard = withCard(bodyCardFilm);
 
 export const Films = () => {
 	const contenCards = useSelector(state => state.app.films);
 	console.log(contenCards);
-	return <main>{<FilmsCard contenCards={contenCards} name="title" />}</main>;
+	return (
+		<main>
+			{
+				<>
+					<FilmsCard contenCards={contenCards} name="title" />
+					<ToastContainer />
+				</>
+			}
+		</main>
+	);
 };
