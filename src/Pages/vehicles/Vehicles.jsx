25f10b5c67ebda5b7635ba80e90c4ce40618ff39
Sergey Kulazhenko <skulazhanka@exadel.com>
2021-07-23 12:55:00 +0300

    feat: Created a popup for component Vihicles

--- src/Pages/vehicles/Vehicles.jsx
+++ src/Pages/vehicles/Vehicles.jsx
@@ -3,20 +3,24 @@ import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { ButtonLoadMore } from "../../components";
 import { bodyCardVehicles } from "../../components/bodyCards/bodyCardVehicles";
+import { Popup } from "../../components/popup/Popup";
 
 import { withCard } from "../../hoc/withCardPeople";
 import { GET_MORE_VEHICLES } from "../../redux/actions/actions";
+import { VehiclesFragmentPopup } from "./VehiclesFragmentPopup";
 
 const VehiclesCards = withCard(bodyCardVehicles);
 
 export const Vehicles = () => {
 	const contenCards = useSelector(state => state.app.vehicles);
 	const isLoadedVehicles = useSelector(state => state.app.isLoadedVehicles);
+	const { isVisiblePopup } = useSelector(state => state.app);
 
 	return (
 		<main>
 			<VehiclesCards contenCards={contenCards} name="name" />
 			{!isLoadedVehicles && <ButtonLoadMore action={GET_MORE_VEHICLES} />}
+			{isVisiblePopup && <Popup FragmentPopup={VehiclesFragmentPopup} />}
 			<ToastContainer />
 		</main>
 	);
