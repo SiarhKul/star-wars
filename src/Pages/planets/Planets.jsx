9e7ff9fa8630d915e4167f09996bdde0fe6fc90a
Sergey Kulazhenko <skulazhanka@exadel.com>
2021-07-23 11:37:12 +0300

    feat: Created a popup for component Planets

--- src/Pages/planets/Planets.jsx
+++ src/Pages/planets/Planets.jsx
@@ -4,20 +4,23 @@ import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { ButtonLoadMore } from "../../components";
 import { bodyCardPlanets } from "../../components/bodyCards/bodyCardPlanets";
+import { Popup } from "../../components/popup/Popup";
 import { withCard } from "../../hoc/withCardPeople";
 import { GET_MORE_PLANETS } from "../../redux/actions/actions";
+import { PlanetsFragmentPopup } from "./PlanetsFragmentPopup";
 
 const PlanetsCards = withCard(bodyCardPlanets);
 
 export const Planets = () => {
 	const contenCards = useSelector(state => state.app.planets);
 	const isLoadedPlanets = useSelector(state => state.app.isLoadedPlanets);
+	const isVisiblePopup = useSelector(state => state.app.isVisiblePopup);
 
 	return (
 		<main className="main-people">
 			<PlanetsCards contenCards={contenCards} name="name" />
-
 			{!isLoadedPlanets && <ButtonLoadMore action={GET_MORE_PLANETS} />}
+			{isVisiblePopup && <Popup FragmentPopup={PlanetsFragmentPopup} />}
 			<ToastContainer />
 		</main>
 	);
