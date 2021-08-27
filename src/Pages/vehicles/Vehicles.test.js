0ad86b752802afc6ea480f7d19d23bad7e398f1b
Siarhei Kulazhanka <skulazhanka@exadel.com>
2021-08-27 16:52:25 +0300

    feat: Added test Vehicles component

--- src/Pages/vehicles/Vehicles.test.js
+++ src/Pages/vehicles/Vehicles.test.js
@@ -1,61 +1,62 @@
+import { mount } from "enzyme";
 import React from "react";
 import { Provider } from "react-redux";
-import { Starships } from "./Starships";
+import { Vehicles } from "./Vehicles";
 import configureStore from "redux-mock-store";
 
 const mockStore = configureStore();
 
-describe("Test <Starships/>", () => {
-	let component;
+describe("Test <Vehicles/>", () => {
 	let store;
+	let component;
 
 	const getMockInitialStore = showButton => {
 		return {
 			dataFromServer: {
-				starships: [],
+				vehicles: [],
 			},
 			loading: {
-				isLoadedStarships: showButton,
+				isLoadedVehicles: showButton,
 			},
 			router: {
 				location: {
-					pathname: "/starships",
+					pathname: "/vehicles",
 				},
 			},
 		};
 	};
 
-	const starhips = () =>
+	const starships = () =>
 		mount(
 			<Provider store={store}>
-				<Starships />
+				<Vehicles />
 			</Provider>
 		);
 
 	beforeEach(() => {
 		store = mockStore(getMockInitialStore(true));
-		component = starhips();
+		component = starships();
 	});
 
-	it("render snapshot <Starships/> ", () => {
+	it("render snapshot <Vehicles/>", () => {
 		expect(component).toMatchSnapshot();
 	});
 
-	it("should render ButtonLoadMore in <Starships/> if  all data has been loaded", () => {
+	it("should render ButtonLoadMore in <Vehicles/> if  all data has been loaded", () => {
 		expect(component.find("ButtonLoadMore").exists()).toBeFalsy();
 	});
 
 	it("should render ButtonLoadMore in <Starships/> if  all data has not been loaded", () => {
 		store = mockStore(getMockInitialStore(false));
-		component = starhips();
-		expect(component.find("ButtonLoadMore").exists()).toBeTruthy;
+		component = starships();
+		expect(component.find("ButtonLoadMore").exists()).toBeTruthy();
 	});
 
-	it("should render alert message in <People/>", () => {
+	it("should render alert message in <Starships/>", () => {
 		expect(component.find("ToastContainer")).toHaveLength(1);
 	});
 
-	it("should render <People/>", () => {
+	it("should render <Starships/>", () => {
 		expect(component.find("Cards")).toHaveLength(1);
 	});
 });
