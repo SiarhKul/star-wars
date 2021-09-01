c6ecbe68bbcf024700df9a13d66da70b73d1f65d
Siarhei Kulazhanka <skulazhanka@exadel.com>
2021-09-01 10:01:27 +0300

    feat: Added noFound page

---
+++ src/components/noFound/noFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import error404 from "../../assets/404.png";
+
+export const noFound = () => {
+	return (
+		<div className="wrapper-error404">
+			<img className="wrapper-error404__img" src={error404} alt="error404" />
+		</div>
+	);
+};
