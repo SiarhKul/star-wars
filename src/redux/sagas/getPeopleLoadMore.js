bc3a8e3163da3aa1a78225221190f481e7633e29
Siarhei Kulazhanka <skulazhanka@exadel.com>
2021-09-07 10:09:35 +0300

    feat: Added test sagas getPeople and getPeopleLoad more

--- src/redux/sagas/getPeopleLoadMore.js
+++ src/redux/sagas/getPeopleLoadMore.js
@@ -9,16 +9,17 @@ import {
 	SET_PEOPLE_TO_STORE,
 } from "../actions/actions";
 
-export function* watchLoadMoreDataPeople() {
-	yield takeEvery(GET_MORE_PEOPLE, workerGetPeopleMore);
-}
-
 export function* workerGetPeopleMore() {
 	const pageNumber = counterPage("people");
 	const data = yield call(getMoreResources, URL_GET_MORE_PEOPLE, pageNumber);
 
 	yield put({ type: SET_PEOPLE_TO_STORE, payload: data.results });
-	if (typeof data.next !== "string") {
+
+	if (data.next === null) {
 		yield put({ type: IS_PEOPLE_LOADED });
 	}
+}
+
+export function* watchLoadMoreDataPeople() {
+	yield takeEvery(GET_MORE_PEOPLE, workerGetPeopleMore);
 }
