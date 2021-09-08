b8099ac5774ab071b1bc246a2e4d8da278b073fa
SiarhKul <SiarhKul@gmail.com>
2021-09-08 15:20:33 +0300

    refactor: Replace many sagas worker with one worker

--- src/redux/sagas/getPeople.js
+++ src/redux/sagas/getPeople.js
@@ -1,28 +1,26 @@
 import { LOCATION_CHANGE } from "connected-react-router";
-import { put, call, take, fork, select } from "redux-saga/effects";
+import { put, call, take, select } from "redux-saga/effects";
 import { getResources } from "../../API/getResources";
-import { URL_GET_PEOPLE } from "../../API/urls";
-import { SET_PEOPLE_TO_STORE } from "../actions/actions";
 import { isDataLoadedFromServer } from "../actionsCreators/actionsCreators";
 import { selectState } from "../selectors/selectors";
 
-export function* workerGetPeople() {
-	yield put(isDataLoadedFromServer(true));
-	const data = yield call(getResources, URL_GET_PEOPLE);
-	yield put({ type: SET_PEOPLE_TO_STORE, payload: data.results });
-	yield put(isDataLoadedFromServer(false));
-}
-
-export function* watchLoadDataPeople() {
+export function* watchLoadData(url, urlGET, reduxAction) {
 	while (true) {
 		const { payload } = yield take(LOCATION_CHANGE);
 		const store = yield select(selectState);
 
 		if (
-			payload.location.pathname.endsWith("/people") &
-			(store.dataFromServer.people.length === 0)
+			payload.location.pathname.endsWith(url) &
+			(store.dataFromServer[url].length === 0)
 		) {
-			yield call(workerGetPeople);
+			yield call(workerGet, urlGET, reduxAction);
 		}
 	}
+}
+
+export function* workerGet(urlGET, reduxAction) {
+	yield put(isDataLoadedFromServer(true));
+	const data = yield call(getResources, urlGET);
+	yield put({ type: reduxAction, payload: data.results });
+	yield put(isDataLoadedFromServer(false));
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,26 +1,46 @@
 import { fork } from "redux-saga/effects";
-import { watchLoadDataFilms } from "./getFilms";
-import { watchLoadDataPeople } from "./getPeople";
+
+import {
+	URL_GET_FILMS,
+	URL_GET_PEOPLE,
+	URL_GET_PLANETS,
+	URL_GET_SPECIES,
+	URL_GET_STARSHIPS,
+	URL_GET_VEHICLE,
+} from "../../API/urls";
+
+import {
+	SET_FILMS_TO_STORE,
+	SET_PEOPLE_TO_STORE,
+	SET_PLANETS_TO_STORE,
+	SET_SPECIES_TO_STORE,
+	SET_STARSHIPS_TO_STORE,
+	SET_VEHICLES_TO_STORE,
+} from "../actions/actions";
+
+import { watchLoadData } from "./getPeople";
 import { watchLoadMoreDataPeople } from "./getPeopleLoadMore";
-import { watchLoadDataPlanets } from "./getPlanets";
 import { watchLoadMoreDataPlanets } from "./getPlanetsLoadMore";
-import { watcherLoadDataSpecies } from "./getSpecies";
 import { watcherLoadMoreDataSpecies } from "./getSpeciesLoadMore";
-import { watchLoadDataStarships } from "./getStarships";
 import { watchLoadMoreDataStarships } from "./getStarshipsLoadMore";
-import { watchLoadDataVehicles } from "./getVehicles";
 import { watchLoadMoreDataVehicles } from "./getVehiclesLoadMore";
 
 export default function* rootSaga() {
-	yield fork(watchLoadDataPeople);
+	yield fork(watchLoadData, "films", URL_GET_FILMS, SET_FILMS_TO_STORE);
+	yield fork(watchLoadData, "people", URL_GET_PEOPLE, SET_PEOPLE_TO_STORE);
+	yield fork(watchLoadData, "planets", URL_GET_PLANETS, SET_PLANETS_TO_STORE);
+	yield fork(
+		watchLoadData,
+		"starships",
+		URL_GET_STARSHIPS,
+		SET_STARSHIPS_TO_STORE
+	);
+	yield fork(watchLoadData, "vehicles", URL_GET_VEHICLE, SET_VEHICLES_TO_STORE);
+	yield fork(watchLoadData, "species", URL_GET_SPECIES, SET_SPECIES_TO_STORE);
+
 	yield fork(watchLoadMoreDataPeople);
-	yield fork(watchLoadDataPlanets);
 	yield fork(watchLoadMoreDataPlanets);
-	yield fork(watchLoadDataStarships);
-	yield fork(watchLoadMoreDataStarships);
-	yield fork(watchLoadDataFilms);
-	yield fork(watchLoadDataVehicles);
 	yield fork(watchLoadMoreDataVehicles);
-	yield fork(watcherLoadDataSpecies);
+	yield fork(watchLoadMoreDataStarships);
 	yield fork(watcherLoadMoreDataSpecies);
 }
